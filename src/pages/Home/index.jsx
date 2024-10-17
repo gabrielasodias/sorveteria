bf77de73bf4f3af570041165be7c081829f5e22e
Gabriela <gabrieladiasofc@gmail.com>
2024-10-17 17:39:03 -0300

    Add HTTP methods, interaction with backend

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,34 +1,52 @@
+import { useEffect, useState, useRef } from 'react'
 import './style.css'
 import Trash from '../../assets/trash.svg'
+import api  from '../../services/api'
 
 function Home() {
 
-  const sabores = [{
-    id: 1,
-    tipoSorvete: 'Picolé',
-    sabor: 'Chocolate',
-    quantidade: 10
-  }, {
-    id: 2,
-    tipoSorvete: 'Pote',
-    sabor: 'Morango',
-    quantidade: 15
-  }, {
-    id: 3,
-    tipoSorvete: 'Picolé',
-    sabor: 'Abacaxi',
-    quantidade: 20
+  const [sabores, setSabores] = useState([])
+
+  const inputTipoSorvete = useRef()
+  const inputSabor = useRef()
+  const inputQuantidade = useRef()
+
+  async function getSabores() {
+    const saboresFromApi = await api.get('/sabores')
+
+    setSabores(saboresFromApi.data)
   }
-  ]
+
+  async function createSabores() {
+    await api.post('/sabores', {
+      tipoSorvete: inputTipoSorvete.current.value,
+      sabor: inputSabor.current.value,
+      quantidade: parseInt(inputQuantidade.current.value, 10)
+    }) 
+
+    getSabores()
+
+  }
+
+  async function deleteSabor(id) {
+    await api.delete(`/sabores/${id}`)
+
+    getSabores()
+  }
+
+  useEffect(() => {
+    getSabores()
+  }, [])
+
   return (
 
     <div className='container'>
       <form action>
         <h1>Controle de Estoque</h1>
-        <input placeholder='Sorvete' name='tipoSorvete' type='text' />
-        <input placeholder='Sabor' name='sabor' type='text' />
-        <input placeholder='Quantidade' name='quantidade' type='number' />
-        <button type='button'>Adicionar</button>
+        <input placeholder='Sorvete' name='tipoSorvete' type='text' ref={inputTipoSorvete}/>
+        <input placeholder='Sabor' name='sabor' type='text' ref={inputSabor}/>
+        <input placeholder='Quantidade' name='quantidade' type='number' ref={inputQuantidade}/>
+        <button type='button' onClick={createSabores}>Adicionar</button>
       </form>
 
       {sabores.map(sabor => (
@@ -38,7 +56,7 @@ function Home() {
             <p>Sabor: <span>{sabor.sabor}</span></p>
             <p>Quantidade: <span>{sabor.quantidade}</span> </p>
           </div>
-          <button>
+          <button onClick={() => deleteSabor(sabor.id)}>
             <img src={Trash} alt='Excluir' />
           </button>
         </div>
