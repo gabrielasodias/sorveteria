383f548a2c39d09e641a10cdcc3e9357f6ccf1b3
Gabriela <gabrieladiasofc@gmail.com>
2024-11-12 17:21:10 -0300

    Change folders

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,77 +1,11 @@
-import { useEffect, useState, useRef } from 'react'
 import './style.css'
-import Trash from '../../assets/trash.svg'
-import api  from '../../services/api'
 
 function Home() {
-
-  const [sabores, setSabores] = useState([])
-
-  const inputTipoSorvete = useRef()
-  const inputSabor = useRef()
-  const inputQuantidade = useRef()
-
-  async function getSabores() {
-    const saboresFromApi = await api.get('/sabores')
-
-    setSabores(saboresFromApi.data)
-  }
-
-  async function createSabores() {
-    await api.post('/sabores', {
-      tipoSorvete: inputTipoSorvete.current.value,
-      sabor: inputSabor.current.value,
-      quantidade: parseInt(inputQuantidade.current.value, 10)
-    }) 
-
-    getSabores()
-
-  }
-
-  async function deleteSabor(id) {
-    await api.delete(`/sabores/${id}`)
-
-    getSabores()
-  }
-
-  useEffect(() => {
-    getSabores()
-  }, [])
-
-  return (
-
-    <div className='container'>
-      <form action>
-        <h1>Controle de Estoque</h1>
-        <select name="tipoSorvete" ref={inputTipoSorvete}>
-          <option value="">Selecione o sorvete</option>
-          <option value="Potinho">Potinho</option>
-          <option value="Picolé">Picolé</option>
-          <option value="Skimo">Skimo</option>
-          <option value="Moreninha">Moreninha</option>
-          <option value="Paleta Mexicana">Paleta Mexicana</option>
-        </select>
-        <input placeholder='Sabor' name='sabor' type='text' ref={inputSabor}/>
-        <input placeholder='Quantidade' name='quantidade' type='number' ref={inputQuantidade}/>
-        <button type='button' onClick={createSabores}>Adicionar</button>
-      </form>
-
-      {sabores.map(sabor => (
-        <div key={sabor.id} className='card'>
-          <div>
-            <p>Tipo de Sorvete: <span>{sabor.tipoSorvete}</span> </p>
-            <p>Sabor: <span>{sabor.sabor}</span></p>
-            <p>Quantidade: <span>{sabor.quantidade}</span> </p>
-          </div>
-          <button onClick={() => deleteSabor(sabor.id)}>
-            <img src={Trash} alt='Excluir' />
-          </button>
+    return (
+        <div className="container">
+            <h1>Home</h1>
         </div>
-      ))}
-
-    </div>
-
-  )
+    )
 }
 
 export default Home
