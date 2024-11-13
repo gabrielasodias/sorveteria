6a94779bc92c91b8c6f9ea955e4c88844e2b7877
Gabriela <gabrieladiasofc@gmail.com>
2024-11-13 13:18:30 -0300

    Header css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,34 @@
+import { useState } from 'react'
 import './Header.css'
+import Logo from '/public/sunny.png'
 
 export default function Header() {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen (!menuOpen);
+    }
+
     return (
+
         <header className="header">
-            <div>
+            <div className='logo'>
+                <img src={Logo} alt="logo Sunny Ice Cream" />
                 <h1>Sunny Ice Cream</h1>
             </div>
 
-            <nav>
+            <button className='menu-button' onClick={toggleMenu}>
+                ☰
+            </button>
+
+            <nav className={`menu ${menuOpen ? "open" : "closed"}`}>
                 <ul>
                     <li><a href="#">Início</a></li>
                     <li><a href="#produtos">Produtos</a></li>
                     <li><a href="#sobre">Sobre</a></li>
                     <li><a href="#contato">Contato</a></li>
-                    <li><a href="#login">Login</a></li>
+                    <li><a href="#login" className='login'>Login</a></li>
                 </ul>
             </nav>
         </header>
