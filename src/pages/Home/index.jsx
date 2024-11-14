3330d0c7943e4a580b1741485cf310670d53be06
Gabriela <gabrieladiasofc@gmail.com>
2024-11-14 15:33:55 -0300

    Home section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,24 @@
 import './style.css'
 import Header from '../../components/header/Header'
+import iceCream1 from '../../assets/ices/ice6.png'
 
 function Home() {
     return (
         <>
             <Header />
+
+            <section className='home-section'>
+                <div className='home-container'>
+                    <div className='home-text'>
+                        <h1>Sunny Ice Cream</h1>
+                        <h2>Esse é o sabor do verão!</h2>
+                    </div>
+                    
+                    <div className='home-image'>
+                        <img src={iceCream1} alt="sorvete" />
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
